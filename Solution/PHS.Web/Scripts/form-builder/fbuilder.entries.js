b609d49e0278b9272d437d6d9aaa245e7b6e32ff
benjaminng <shen_soonng@hotmail.com>
2017-03-12 16:30:03 +0800

    dynamic add and remove row from sort table

--- Solution/PHS.Web/Scripts/form-builder/fbuilder.entries.js
+++ Solution/PHS.Web/Scripts/form-builder/fbuilder.entries.js
@@ -20,7 +20,11 @@
         });
 
         $('#sortButton').live('click', function () {
-            alert('clicking...')
+            $("#sortTable").append('<tr><td width="30%"></td><td width="20%"><select id="test"><option value="Select a value"></option><option value="ASC">ASC</option><option value="DESC">DESC</option></select></td><td width="50%"><a id="removeButton" name="removeButton" class="hyperlink-button light-blue-button">-</a></td></tr>');
+        });
+
+        $('#removeButton').live('click', function () {
+            $(this).parent().parent().remove();
         });
     }
 
