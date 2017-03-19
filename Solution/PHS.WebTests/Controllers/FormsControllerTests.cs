47456448fcde0a9828762424c6228207fa3c9039
benjaminng <shen_soonng@hotmail.com>
2017-03-19 16:54:04 +0800

    unit test proj

---
+++ Solution/PHS.WebTests/Controllers/FormsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PHS.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHS.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class FormsControllerTests
+    {
+        [TestMethod()]
+        public void ExportToExcelTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
