4b97218f09c3fa4cd9834a0659de26f880b2aaed
yeojc <yeojc@NCS-191016CP7>
2017-03-27 00:34:32 +0800

    Modality Circles
    Circular buttons

---
+++ Solution/PHS.Business/ViewModel/PatientJourney/ModalityCircleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PHS.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHS.Business.ViewModel.PatientJourney
+{
+    public class ModalityCircleViewModel
+    {
+        public string Name {get; set;} 
+        public int Position { get; set; }
+        public string IconPath { get; set; }
+        public bool Active { get; set; }
+        public bool Visible { get; set; }
+
+    }
+}
--- Solution/PHS.Web/Controllers/PatientJourneyController.cs
+++ Solution/PHS.Web/Controllers/PatientJourneyController.cs
@@ -84,5 +84,45 @@ namespace PHS.Web.Controllers
 
             return View(result);
         }
+
+
+        public ActionResult _JourneyModalityCirclesPartial()
+        {
+            ModalityCircleViewModel modalityCircle = new ModalityCircleViewModel();
+            List<ModalityCircleViewModel> modalityCircleList = new List<ModalityCircleViewModel>();
+
+            modalityCircle.Name = "History Taking";
+            modalityCircle.Position = 0;
+            modalityCircle.Active = true;
+            modalityCircle.Visible = true;
+            modalityCircle.IconPath = "../../Content/images/Modality/abacus.png";
+            modalityCircleList.Add(modalityCircle);
+
+            modalityCircle = new ModalityCircleViewModel();
+            modalityCircle.Name = "Registration";
+            modalityCircle.Position = 1;
+            modalityCircle.Active = true;
+            modalityCircle.Visible = true;
+            modalityCircle.IconPath = "../../Content/images/Modality/achievement.png";
+            modalityCircleList.Add(modalityCircle);
+
+            modalityCircle = new ModalityCircleViewModel();
+            modalityCircle.Name = "FIT";
+            modalityCircle.Position = 2;
+            modalityCircle.Active = false;
+            modalityCircle.Visible = true;
+            modalityCircle.IconPath = "../../Content/images/Modality/agenda.png";
+            modalityCircleList.Add(modalityCircle);
+
+            modalityCircle = new ModalityCircleViewModel();
+            modalityCircle.Name = "TeleHealth";
+            modalityCircle.Position = 3;
+            modalityCircle.Active = true;
+            modalityCircle.Visible = false;
+            modalityCircle.IconPath = "../../Content/images/Modality/balance.png";
+            modalityCircleList.Add(modalityCircle);
+
+            return View(modalityCircleList); 
+        }
     }
 }
