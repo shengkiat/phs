329ef01f40a97ce0ba79758ec2b8f6ed704e8ac3
Goh Chong Jhee <base5-github@yahoo.com>
2017-03-30 22:17:17 +0800

    Added Modality Controller

---
+++ Solution/PHS.Web/Controllers/ModalityController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PHS.Web.Controllers
+{
+    public class ModalityController : Controller
+    {
+        // GET: Modality
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Modality/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Modality/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Modality/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Modality/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Modality/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Modality/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Modality/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
