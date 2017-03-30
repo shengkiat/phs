c1dff1dd29d5630674152c4baa065770989fe1da
Goh Chong Jhee <base5-github@yahoo.com>
2017-03-30 22:06:42 +0800

    Add ViewModel for Event Modality

---
+++ Solution/PHS.DB/ViewModels/eventIndexdata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHS.DB.ViewModels
+{
+    public class eventIndexdata
+    {
+        public IEnumerable<@event> events { get; set; }
+        public IEnumerable<Modality> modalities { get; set; }
+        public IEnumerable<form> forms { get; set; }
+    }
+}
