d4c7a28f11d3ff2b48b3d1f7ae111d0c8a0c9a2c
benjaminng <shen_soonng@hotmail.com>
2017-04-01 21:30:48 +0800

    setup for event patient db

---
+++ Solution/PHS.Repository/Repository/EventPatientRepository.cs
@@ -0,0 +1,14 @@
+﻿using PHS.DB;
+using PHS.Repository.Interface;
+using PHS.Repository.Repository.Core;
+using System.Data.Entity;
+
+namespace PHS.Repository.Repository
+{
+    public class EventPatientRepository : Repository<EventPatient>, IEventPatientRepository
+    {
+        public EventPatientRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
