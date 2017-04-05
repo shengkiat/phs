35736e97e8966ef18808cf697cc00f137f828b1e
shengkiat <shengkiat@gmail.com>
2017-04-05 21:22:42 +0800

    Change initial route to login

--- Solution/PHS.Web/App_Start/RouteConfig.cs
+++ Solution/PHS.Web/App_Start/RouteConfig.cs
@@ -115,7 +115,7 @@ namespace PHS.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "forms", action = "index", id = UrlParameter.Optional }
+                defaults: new { controller = "home", action = "login", id = UrlParameter.Optional }
             );
 
 
