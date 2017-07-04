dd700b3e48703df141675c349b19b5e5c472522c
benjaminng <shen_soonng@hotmail.com>
2017-07-04 17:30:04 +0800

    base business test project

---
+++ Solution/PHS.BusinessTests/Implementation/FormManagerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PHS.Business.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PHS.Business.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PHS.DB;
+using PHS.Repository;
+using PHS.Repository.Context;
+using System.IO;
+using PHS.DB.ViewModels.Forms;
+using PHS.Business.Extensions;
+using System.Transactions;
+using System.Web.Mvc;
+using PHS.Common;
+
+namespace PHS.Business.Implementation.Tests
+{
+    [TestClass()]
+    public class FormManagerTests
+    {
+        [TestMethod()]
+        public void FindAllTemplates()
+        {
+            FormManager target = new FormManager();
+            target.FindAllTemplates();
+        }
+    }
+}
