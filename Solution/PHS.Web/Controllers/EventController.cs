8845f2af7f8dab475edc87afa6496debcaceaf6e
richardz316 <jsman316@gmail.com>
2017-07-05 02:42:53 +0800

    Event Management - add Modality table in UI - ajust other UI elements - Change controller to create Event with default Modalities

--- Solution/PHS.Web/Controllers/EventController.cs
+++ Solution/PHS.Web/Controllers/EventController.cs
@@ -48,26 +48,32 @@ namespace PHS.Web.Controllers
 
         public ActionResult Create()
         {
-            List<Template> templates;
-            using (var formManager = new FormManager())
-            {
-                templates = formManager.FindAllTemplates();
+            //List<Template> templates;
+            //using (var formManager = new FormManager())
+            //{
+            //    templates = formManager.FindAllTemplates();
 
-                ViewData["ss"] = templates;
+            //    ViewData["ss"] = templates;
 
-                String htmlString = "<select id=\"SelectedForm\" name=\"Modalities[0].FormID\">";
-               // String htmlString = "";
-                foreach (var template in templates)
-                {
-                    var templateView = TemplateViewModel.CreateFromObject(template);
-                    htmlString += "<option value=\"" + template.TemplateID + "\">" + templateView.Title + "</option>";
-                }
+            //    String htmlString = "<select id=\"SelectedForm\" name=\"Modalities[0].FormID\">";
+            //    // String htmlString = "";
+            //    foreach (var template in templates)
+            //    {
+            //        var templateView = TemplateViewModel.CreateFromObject(template);
+            //        htmlString += "<option value=\"" + template.TemplateID + "\">" + templateView.Title + "</option>";
+            //    }
 
-                htmlString += "</select>";
+            //    htmlString += "</select>";
 
-                ViewData["Forms"] = htmlString;
-            }
-            return View();
+            //    ViewData["Forms"] = htmlString;
+            //}
+
+            //Create default Modalities
+            List<Modality> modalities = BuildDefaultModalites();
+            PHSEvent phsEvent = new PHSEvent();
+            phsEvent.Modalities = modalities;
+            
+            return View(phsEvent);
         }
 
         [HttpPost]
@@ -95,7 +101,31 @@ namespace PHS.Web.Controllers
 
 
 
+        private List<Modality> BuildDefaultModalites()
+        {
+            List<Modality> modalities = new List<Modality>();
+            Modality mRegister = new Modality();
+            mRegister.Name = "Registration";
+            mRegister.Position = 0;
+            mRegister.IconPath = "../../../Content/images/Modality/01registration.png";
+            mRegister.IsActive = false;
+            mRegister.IsVisible = true;
+            mRegister.HasParent = false;
+            mRegister.Status = "Pending";
+            modalities.Add(mRegister);
 
+            Modality mHistoryTaking = new Modality();
+            mHistoryTaking.Name = "History Taking";
+            mHistoryTaking.Position = 0;
+            mHistoryTaking.IconPath = "../../../Content/images/Modality/02historytaking.png";
+            mHistoryTaking.IsActive = false;
+            mHistoryTaking.IsVisible = true;
+            mHistoryTaking.HasParent = false;
+            mHistoryTaking.Status = "Pending";
+            modalities.Add(mHistoryTaking);
+
+            return modalities; 
+        }
 
 
 
