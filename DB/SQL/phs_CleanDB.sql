fddf4ea41ee6a34213f347713405e7d206fe9836
richardz316 <jsman316@gmail.com>
2017-08-06 12:01:27 +0800

    add back the insert modality sql for 2017 event

--- DB/SQL/phs_CleanDB.sql
+++ DB/SQL/phs_CleanDB.sql
@@ -659,30 +659,30 @@ INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (2, 12)
 GO
 INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (2, 13)
 GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 1)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 2)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 4)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 5)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 6)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 7)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 8)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 9)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 10)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 11)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 12)
---GO
---INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 13)
---GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 1)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 2)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 4)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 5)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 6)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 7)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 8)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 9)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 10)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 11)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 12)
+GO
+INSERT [dbo].[EventModality] ([PHSEventID], [ModalityID]) VALUES (3, 13)
+GO
 
 ---  Forms Sample  --
 SET IDENTITY_INSERT [phs].[dbo].[Form] ON 
