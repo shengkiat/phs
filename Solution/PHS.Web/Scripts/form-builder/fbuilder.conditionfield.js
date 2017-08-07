b804eaba54c7e86629f855749b3d283a5d0a289f
benjaminng <shen_soonng@hotmail.com>
2017-08-07 18:25:15 +0800

    condition field for checkbox

--- Solution/PHS.Web/Scripts/form-builder/fbuilder.conditionfield.js
+++ Solution/PHS.Web/Scripts/form-builder/fbuilder.conditionfield.js
@@ -10,6 +10,7 @@
         }
 
         else if (criteria == "==") {
+
             if (options.includes(selectedValue)) {
                 $(this).attr('style', '');
             }
@@ -21,6 +22,55 @@
 
         else if (criteria == "!=") {
             if (options.includes(selectedValue)) {
+                $(this).attr('style', 'display:none;');
+            }
+
+            else {
+                $(this).attr('style', '');
+            }
+        }
+    });
+}
+
+function displayOrHideFromCheckBox(inputOption, selectedValue, conditionId) {
+
+    $(inputOption).parent().parent().parent().parent().parent().find("[data-reference]=" + conditionId).each(function () {
+        var criteria = $(this).attr('data-criteria');
+        var options = $(this).attr('data-options');
+
+        if (selectedValue == "") {
+            $(this).attr('style', 'display:none;');
+        }
+
+        else if (criteria == "==") {
+            var foundOption = false;
+
+            $.each(selectedValue, function (i, val) {
+                if (!foundOption && options.includes(selectedValue[i]))
+                {
+                    foundOption = true;
+                }
+            });
+
+            if (foundOption) {
+                $(this).attr('style', '');
+            }
+
+            else {
+                $(this).attr('style', 'display:none;');
+            }
+        }
+
+        else if (criteria == "!=") {
+            var foundOption = false;
+
+            $.each(selectedValue, function (i, val) {
+                if (!foundOption && options.includes(selectedValue[i])) {
+                    foundOption = true;
+                }
+            });
+
+            if (foundOption) {
                 $(this).attr('style', 'display:none;');
             }
 
