b96d152e68a66d41a43b25313f9fb540356d9913
benjaminng <shen_soonng@hotmail.com>
2017-09-09 15:49:23 +0800

    try to solve security: Bad Practices: Model With Required Non-Nullable Property

--- Solution/PHS.DB/ViewModels/event.cs
+++ Solution/PHS.DB/ViewModels/event.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using PHS.DB.Attributes;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PHS.DB
 {
+    [TrackChangesAttribute]
     [MetadataType(typeof(EventMetadata))]
     public partial class PHSEvent
     {
