47f6e1b0d9a57ef62ff5945c63875b05f648471b
benjaminng <shen_soonng@hotmail.com>
2017-09-13 20:51:32 +0800

    form export business refractor before revamp

---
+++ Solution/PHS.Business/Implementation/FormExportManager.cs
@@ -0,0 +1,284 @@
+﻿using PHS.Business.Extensions;
+using PHS.Business.Interface;
+using PHS.Common;
+using PHS.DB.ViewModels.Form;
+using PHS.FormBuilder.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHS.Business.Implementation
+{
+    public class FormExportManager : BaseFormManager, IFormExportManager, IManagerFactoryBase<IFormExportManager>
+    {
+        public IFormExportManager Create()
+        {
+            return new FormExportManager();
+        }
+
+        public DataTable CreateFormEntriesDataTable(TemplateViewModel model)
+        {
+            using (var unitOfWork = CreateUnitOfWork())
+            {
+                var template = FindTemplate(model.TemplateID.Value);
+                // var form = this._formRepo.GetForm(formId);
+                var templateView = TemplateViewModel.CreateFromObject(template);
+
+                // formView.Entries = formManager.HasSubmissions(formView).ToList();
+                templateView.Entries = unitOfWork.FormRepository.GetTemplateFieldValuesByForm(templateView).ToList();
+
+                templateView.GroupedEntries = templateView.Entries.GroupBy(g => g.EntryId);
+
+                return CreateFormEntriesDataTable(templateView, model.SortFields, model.CriteriaFields);
+            }
+        }
+
+        private DataTable CreateFormEntriesDataTable(TemplateViewModel template, List<SortFieldViewModel> sortFields, List<CriteriaFieldViewModel> criteriaFields)
+        {
+            var dt = new DataTable(template.Title);
+            List<string> columnNames = new List<string>();
+            int columnCount = 0;
+
+            //TODO - Format Export Forms Data for BMI and Address
+            foreach (var field in template.Fields)
+            {
+
+                if (field.FieldType == Constants.TemplateFieldType.MATRIX)
+                {
+                    string[] rows = field.MatrixRow.Split(",");
+                    foreach (string row in rows)
+                    {
+                        dt.Columns.Add(new DataColumn(row));
+
+                        columnCount++;
+                    }
+                }
+                else if (field.FieldType == Constants.TemplateFieldType.ADDRESS)
+                {
+                    dt.Columns.Add(new DataColumn("Blk"));
+                    dt.Columns.Add(new DataColumn("Unit"));
+                    dt.Columns.Add(new DataColumn("Street Address"));
+                    dt.Columns.Add(new DataColumn("ZipCode"));
+
+                    columnCount += 4;
+
+                }
+                else if (field.FieldType == Constants.TemplateFieldType.BMI)
+                {
+                    dt.Columns.Add(new DataColumn("Weight"));
+                    dt.Columns.Add(new DataColumn("Height"));
+                    dt.Columns.Add(new DataColumn("BMI"));
+
+                    columnCount += 3;
+                }
+                else
+                {
+                    var colName = field.Label;
+
+                    if (dt.Columns.Contains(colName))
+                    {
+                        dt.Columns.Add(new DataColumn(columnCount + ": " + colName));
+                    }
+                    else
+                    {
+                        dt.Columns.Add(new DataColumn(colName));
+                    }
+
+                    columnCount++;
+                }
+            }
+
+            // Comment out original
+
+            //foreach (var field in form.GroupedEntries.FirstOrDefault())
+            //{
+            //    if (field.FieldType != Constants.FieldType.HEADER)
+            //    {
+            //        var colName = field.FieldLabel;
+            //        if (columnNames.Any(cn => cn.IsTheSameAs(colName)))
+            //        {
+            //            int colNumber = 1;
+            //            do
+            //            {
+            //                colName = string.Format("{0} ({1})", colName, colNumber);
+            //                colNumber++;
+
+            //            } while (columnNames.Any(cn => cn.IsTheSameAs(colName)));
+            //        }
+            //        columnNames.Add(colName);
+            //        dt.Columns.Add(new DataColumn(colName));
+            //    }
+            //    columnCount++;
+            //}
+
+            dt.Columns.Add(new DataColumn("Submitted On"));
+
+            foreach (var group in template.GroupedEntries)
+            {
+                DataRow row = dt.NewRow();
+                var fieldAddedOn = group.FirstOrDefault().DateAdded;
+                int columnIndex = 0;
+
+                foreach (var entry in group)
+                {
+                    // for (columnIndex = 0; columnIndex < columnCount; )
+                    //  {
+                    if (entry.FieldType == Constants.TemplateFieldType.MATRIX)
+                    {
+                        var matrixField = entry.Value;
+
+                        string[] submissions = matrixField.Split(",");
+                        foreach (string submitValue in submissions)
+                        {
+                            row[columnIndex] = submitValue;
+                            columnIndex++;
+                        }
+
+                        columnIndex--;
+                    }
+                    else if (entry.FieldType == Constants.TemplateFieldType.ADDRESS)
+                    {
+                        var addressField = entry.Value;
+
+                        AddressViewModel address = addressField.FromJson<AddressViewModel>();
+
+                        row[columnIndex] = address.Blk;
+                        row[columnIndex + 1] = address.Unit;
+                        row[columnIndex + 2] = address.StreetAddress;
+                        row[columnIndex + 3] = address.ZipCode;
+
+                        columnIndex += 4;
+                    }
+                    else if (entry.FieldType == Constants.TemplateFieldType.BMI)
+                    {
+                        var bmiField = entry.Value;
+
+                        BMIViewModel bmi = bmiField.FromJson<BMIViewModel>();
+
+                        row[columnIndex] = bmi.Weight;
+                        row[columnIndex + 1] = bmi.Height;
+                        row[columnIndex + 2] = bmi.BodyMassIndex;
+
+                        columnIndex += 3;
+                    }
+                    else if (columnIndex < group.Count())
+                    {
+                        var field = group.ElementAt(columnIndex);
+                        row[columnIndex] = field.Format(true);
+                        columnIndex++;
+                    }
+                    else
+                    {
+
+                        row[columnIndex] = entry.Value;
+                        columnIndex++;
+                    }
+                    // }
+                }
+
+                row[columnIndex] = fieldAddedOn.ToString("yyyy-MM-dd HH:mm");
+                dt.Rows.Add(row);
+            }
+
+
+
+            DataView dv = new DataView(dt);
+            dv.RowFilter = GenerateFlitering(criteriaFields);
+            dv.Sort = GenerateSorting(sortFields);
+
+            return dv.ToTable();
+        }
+
+        private string GenerateSorting(List<SortFieldViewModel> sortFields)
+        {
+            string result = "";
+
+            if (sortFields != null)
+            {
+                foreach (var sortFieldViewModel in sortFields)
+                {
+                    var sortF = sortFieldViewModel.FieldLabel;
+                    var sortO = sortFieldViewModel.SortOrder;
+                    if (!String.IsNullOrEmpty(sortF) && !String.IsNullOrEmpty(sortO))
+                    {
+                        result += string.Format(", {0} {1}", sortF, sortO);
+                    }
+                }
+            }
+
+            if (result.Length > 1)
+            {
+                result = result.Remove(0, 2);
+            }
+
+            return result;
+        }
+
+        private string GenerateFlitering(List<CriteriaFieldViewModel> criteriaFields)
+        {
+            string result = "";
+
+            if (criteriaFields != null)
+            {
+                foreach (var criteriaField in criteriaFields)
+                {
+                    if (!String.IsNullOrEmpty(criteriaField.FieldLabel)
+                        && !String.IsNullOrEmpty(criteriaField.CriteriaLogic)
+                        && !String.IsNullOrEmpty(criteriaField.CriteriaValue[criteriaField.FieldLabel]))
+                    {
+                        result += string.Format(" OR [{0}] {1}", criteriaField.FieldLabel, getConvertedCriteriaValue(criteriaField.CriteriaLogic, criteriaField.CriteriaValue[criteriaField.FieldLabel]));
+                        if (criteriaField.CriteriaSubFields != null)
+                        {
+                            foreach (var criteriaSubField in criteriaField.CriteriaSubFields)
+                            {
+                                result += string.Format(" {0} [{1}] {2}", criteriaSubField.OperatorLogic, criteriaField.FieldLabel, getConvertedCriteriaValue(criteriaSubField.CriteriaLogic, criteriaSubField.CriteriaValue[criteriaField.FieldLabel]));
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            if (result.Length > 1)
+            {
+                result = result.Remove(0, 4);
+            }
+
+            return result;
+        }
+
+
+        private string getConvertedCriteriaValue(string criteriaLogic, string value)
+        {
+            Dictionary<string, string> mappedValues = new Dictionary<string, string>();
+            mappedValues.Add("eq", "=");
+            mappedValues.Add("neq", "<>");
+            mappedValues.Add("gt", ">");
+            mappedValues.Add("gte", ">=");
+            mappedValues.Add("lt", "<");
+            mappedValues.Add("lte", "<=");
+
+            switch (criteriaLogic)
+            {
+                case "startswith":
+                    return string.Format("LIKE '{0}*'", value);
+                case "endswith":
+                    return string.Format("LIKE '*{0}'", value);
+                case "contains":
+                    return string.Format("LIKE '*{0}*'", value);
+                case "doesnotcontain":
+                    return string.Format("NOT LIKE '*{0}*'", value);
+                case "in":
+                    return string.Format("IN ({0})", value);
+                default:
+                    return string.Format("{0} '{1}'", mappedValues[criteriaLogic], value);
+            }
+
+        }
+
+
+    }
+}
