ab98de51b57cc518dcb6ddb46e0672e8be6c16af
Smita <Smita@DESKTOP-B301COU>
2017-10-03 00:02:44 +0800

    move displayStandardReferenceResult to javascript file Fix radio button standard reference issue

---
+++ Solution/PHS.Web/Scripts/form-builder/fbuilder.displaystdrefresult.js
@@ -0,0 +1,30 @@
+﻿function displayStandardReferenceResult(value, fieldId) {
+    if(value != "" && value != null) {
+        var standardReferenceID = document.getElementById("SubmitFields["+fieldId+"].StandardReferenceId").value
+
+        if (standardReferenceID != "")
+        {
+            var jsonObject = {
+                "standardReferenceId": standardReferenceID,
+                "value": value
+            };
+
+            $.ajax({
+                url: '/phs/FormAccess/GetReferenceRange',
+                type: "POST",
+                data: JSON.stringify(jsonObject),
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                error: function (response) {
+                    document.getElementById("SubmitFields["+fieldId+"].StandardReferenceResult").innerHTML = response.Error;
+                },
+                success: function (response) {
+                    document.getElementById("SubmitFields["+fieldId+"].StandardReferenceResult").innerHTML = response.Status;
+                    if (response.Highlight) {
+                        document.getElementById("SubmitFields["+fieldId+"].StandardReferenceResult").style.color = "red";
+                    }
+                }
+            });
+        }
+    }
+}
