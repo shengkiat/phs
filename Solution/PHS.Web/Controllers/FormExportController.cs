7b88b7e4ed51aed216ebc44e3a52abf787dee4fb
Jia Cheng <jiacheng.yeo@gmail.com>
2017-10-03 18:23:34 +0800

    fix excel export cannot display chiense characters
    fix excel export cannot display chiense characters

--- Solution/PHS.Web/Controllers/FormExportController.cs
+++ Solution/PHS.Web/Controllers/FormExportController.cs
@@ -93,7 +93,10 @@ namespace PHS.Web.Controllers
 
                 Response.ClearContent();
                 Response.AddHeader("content-disposition", "attachment; filename={0}.xls".FormatWith(formExportResult.Title.ToSlug()));
-                Response.ContentType = "application/vnd.ms-excel";
+                //Response.ContentType = "application/vnd.ms-excel";
+                Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+                Response.ContentEncoding = System.Text.Encoding.Unicode;
+                Response.BinaryWrite(System.Text.Encoding.Unicode.GetPreamble());
                 StringWriter sw = new StringWriter();
                 HtmlTextWriter htw = new HtmlTextWriter(sw);
                 gridView.RenderControl(htw);
