5b12ea9cd0764a44d8d4f2ee16be2a22897fb7f2
Smita Bisoyi <smita.bisoyi08@gmail.com>
2017-10-24 01:53:49 +0800

    Add Follow-Up menu

---
+++ Solution/PHS.Web/Controllers/FollowUpController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PHS.Web.Controllers
+{
+    public class FollowUpController : Controller
+    {
+        // GET: FollowUp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
