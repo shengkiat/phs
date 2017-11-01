1f6ce70889c66d72dd98ce7621ceada7e89b4ea7
benjaminng <shen_soonng@hotmail.com>
2017-11-01 19:50:27 +0800

    unit test class

---
+++ Solution/PHS.BusinessTests/Implementation/FollowUpManagerTests.cs
@@ -0,0 +1,66 @@
+﻿using Effort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PHS.Business.Implementation;
+using PHS.BusinessTests;
+using PHS.Repository.Context;
+using PHS.Repository.Interface.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHS.Business.Implementation.Tests
+{
+    [TestClass()]
+    public class FollowUpManagerTests
+    {
+        private FollowUpManager _target;
+        private IUnitOfWork _unitOfWork;
+        private PHSContext _context;
+
+        [TestMethod()]
+        public void ImportCallerTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            var connection = DbConnectionFactory.CreateTransient();
+            _context = new PHSContext(connection);
+            _unitOfWork = new MockUnitOfWork(_context);
+
+            _target = new MockFollowUpManager(_unitOfWork);
+        }
+
+        [TestCleanup]
+        public void CleanupTest()
+        {
+            // dispose of the database and connection
+            _context.Dispose();
+            _unitOfWork.Dispose();
+            _target.Dispose();
+
+            _unitOfWork = null;
+            _context = null;
+            _target = null;
+        }
+
+        private class MockFollowUpManager : FollowUpManager
+        {
+            private IUnitOfWork _unitOfWork;
+
+            public MockFollowUpManager(IUnitOfWork unitOfWork) : base(null)
+            {
+                _unitOfWork = unitOfWork;
+            }
+
+            protected override IUnitOfWork CreateUnitOfWork()
+            {
+                return _unitOfWork;
+            }
+        }
+    }
+}
