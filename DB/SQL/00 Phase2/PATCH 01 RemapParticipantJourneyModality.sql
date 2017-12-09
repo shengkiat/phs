12df82ca8a37ad35b77a2a0c64254053da24b00b
Jia Cheng <jiacheng.yeo@gmail.com>
2017-12-09 13:21:32 +0800

    phase2patching

---
+++ DB/SQL/00 Phase2/PATCH 01 RemapParticipantJourneyModality.sql
@@ -0,0 +1,7 @@
+use [phs]
+
+update ParticipantJourneyModality set TemplateID = 55 where formid = 54
+update ParticipantJourneyModality set TemplateID = 56 where formid = 55
+update ParticipantJourneyModality set TemplateID = 57 where formid = 56
+update ParticipantJourneyModality set TemplateID = 58 where formid = 57
+update ParticipantJourneyModality set TemplateID = 59 where formid = 58
